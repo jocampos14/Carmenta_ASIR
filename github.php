95552e73d76119cef78ec6264bb4b33ad4cafac4
RobertoAsir <95422212+RobertoAsir@users.noreply.github.com>
2021-12-15 15:24:23 +0100

    Subo la versión inicial del proyecto
    Ejercicio de prueba

---
+++ github.php
@@ -0,0 +1,15 @@
+<!doctype html>
+ <html lang="es">
+    <head>
+        <meta charset="utf-8">
+            <title>Título</title>
+    </head>
+    <body>
+         <!-- muestra un número aleatorio de dos -->
+        <?php
+            $numero=mt_rand(0,1);
+            if ($numero==0) echo "<img src='Imagenes/img1.jpg'>";
+            else echo "<img src='Imagenes/img2.jpg'>";
+        ?>
+    </body>
+</html>
